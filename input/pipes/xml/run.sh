8cfcdd08d13e44c1b445bb08e386a387bf828f96
Alexander Ratner <ajratner@gmail.com>
2015-08-09 23:14:22 -0700

    Re-did XML table extractor code to output TSV for cells table, working on #43

--- input/pipes/xml/run.sh
+++ input/pipes/xml/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 [INPUT_XML_DIR] [OUTPUT_JSON_FILE]"
+  echo "Usage: $0 [INPUT_XML_DIR] [OUTPUT_FILE]"
   exit
 fi
 echo "Parsing $1, saving to $2"
